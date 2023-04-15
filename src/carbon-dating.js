844d8d0b0b4dd8676afc4f19e9b7e679709ac863
a.maslov <a.maslov@2gis.ru>
2023-04-16 00:14:29 +0300

    feat: Implement 'Carbon dating' task.

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,17 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function dateSample( sampleActivity ) {
+
+    if (typeof sampleActivity !== 'string')
+      return false;
+
+    const intV = Number.parseFloat(sampleActivity);
+    if (Number.isNaN(intV) || intV <= 0 || intV > MODERN_ACTIVITY)
+      return false;
+
+    return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (.693 / HALF_LIFE_PERIOD));
 }
 
 module.exports = {
